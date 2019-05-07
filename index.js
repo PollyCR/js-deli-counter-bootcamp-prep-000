7c4edcf6278af27374d976917f8d8ad17e1bff3e
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-07 16:49:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber() {
+  var name = ["Ada", "Grace", "Kent"] ;
+  console.log("Welcome, `$name`. You are number `$line` in line.")
+}
+
+function nowServing(katzDeliLine) {
+  return(katzDeliLine[1])
+  
+  if
+}
