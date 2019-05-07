45d237c47643cdaa65ec0d36ccc222b2af7e86b8
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-07 16:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var katzDeliLine = [];
 
 function currentLine() {
   var line = []
-  if (x.length===0)
+  if (x.length===0) {
   return('The line is currently empty.')
 } else {
-  for(var i = 0; i < x; i++)
-line+=
+  for(var i = 0; i < x; i++) 
+}
