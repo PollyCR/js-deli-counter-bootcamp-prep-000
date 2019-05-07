57f4f9f4d73bdfa9e622c6e9781e898d59f0e275
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-07 17:08:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function currentLine(x) {
 } else {
   for(var i = 0; i < x; i++) 
   line += (i + 1) + '.' + x[i] + ','
-  line = line.slice(0, line.length-2)
+  line = line.slice(0, line.length-1)
   return('The line is currently:' + line)
 }
 }
