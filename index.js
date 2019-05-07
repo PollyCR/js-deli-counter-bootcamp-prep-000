6b1b0abb52f4987bba23b714c26b7b4b27d3f293
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-07 17:09:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var katzDeliLine = [];
 
 function currentLine(x) {
-  var line = []
-  if (x.length===0) {
-  return('The line is currently empty.')
-} else {
-  for(var i = 0; i < x; i++) 
-  line += (i + 1) + '.' + x[i] + ','
-  line = line.slice(0, line.length-1)
-  return('The line is currently:' + line)
-}
+    var line = []
+    if (x.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < x.length; i++) {
+        line += (i + 1) + ". " + x[i] + ", "
+      }
+      line = line.slice(0, line.length-2)
+      return "The line is currently: " + line
+    }
 }
 
 function takeANumber(katzDeliLine,name) {
