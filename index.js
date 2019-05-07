41536ae02552c260e777429357ee6f7fbb266060
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-07 16:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function takeANumber() {
-  var name = ["Ada", "Grace", "Kent"] ;
-  console.log("Welcome, `$name`. You are number `$line` in line.")
-}
-
-function nowServing(katzDeliLine) {
-  return(katzDeliLine[1])
-  
-  if
-}
+var = katzDeliLine[]
